571b8b67e434e10dfe895be9fff80bf8625b6070
AryanSharma05 <94731452+AryanSharma05@users.noreply.github.com>
2024-07-24 11:32:26 +0530

    Initial commit

---
+++ scripts/bubbleSort.js
@@ -0,0 +1,13 @@
+const bubbleSort = async(n)=>{
+    for(i=0;i<n-1;i++){
+        for(j=0;j<n-1-i;j++){
+            showPicked(j,j+1);
+            await wait(1500/n);
+            swapById(j,j+1)
+                document.getElementById('cnt').innerText=cnt;
+            remPicked(j,j+1);
+        }
+    correctPos(n-1-i);
+    }
+    correctPos(n-1-i);
+}
